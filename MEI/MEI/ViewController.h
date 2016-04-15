61bf9ae15917712eb5b3e026c71dd79d5bacf3e2
xmg <xmg@163.com>
2016-04-15 09:20:04 +0800

    nii

---
+++ MEI/MEI/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  MEI
+//
+//  Created by zgb13790090648 on 16/4/15.
+//  Copyright © 2016年 zgb13790090648. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+/**<#name#> */
+@property (nonatomic,strong)NSString *name;
+
+@end
+
